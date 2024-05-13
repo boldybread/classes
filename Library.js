f37b72bc0a793d71f913a5eda5b49a30491026ee
Bradley A <138368512+boldybread@users.noreply.github.com>
2024-05-13 13:22:56 +1000

    feat: static class functions

---
+++ Library.js
@@ -0,0 +1,16 @@
+
+
+class Library {
+
+    constructor(){
+
+        this.books = [];
+        this.audiobooks = [];
+        this.games = [];
+        this.movies = [];
+        this.music = [];
+    }
+
+}
+
+module.exports = {Library}
